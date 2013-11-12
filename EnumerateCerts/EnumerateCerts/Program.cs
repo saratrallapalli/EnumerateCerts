407f3824c36d9d5e452cad03edabc2c37084875a
saratrallapalli <saratrallapalli@gmail.com>
2013-11-12 13:37:29 -0800

    Basic enumeration functionality.
    First commit.

---
+++ EnumerateCerts/EnumerateCerts/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace EnumerateCerts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Open a certificate store.
+            X509Store store = new X509Store("My", StoreLocation.CurrentUser);
+
+            foreach (X509Certificate2 certificate in store.Certificates)
+            {
+                Console.WriteLine("Store contains cert with thumbprint: " + certificate.Thumbprint);
+            }
+        }
+    }
+}
